c5f0306ab5c2c22e5d6c7066b04c732bf84d5e2a
llt3055 <llt30555@gmail.com>
2026-05-13 19:48:40 -0700

    continue debug

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -22,56 +22,22 @@ int main() {
     
     // 1. Read data from file into vector, list, and BST
     ifstream fin1("codes.txt");
-    if (!fin1) { cout << "Error opening codes.txt"; return 1; }
+    if (!fin) { cout << "Error opening codes.txt"; return 1; }
 
     // Vector read 
     auto start = high_resolution_clock::now();
-    ifstream fin1("codes.txt");
-    if (!fin1) { cout << "Error opening codes.txt"; return 1; }
-    while (getline(fin1, line)) {
-        v.push_back(line);
+    while (getline(fin, line)) {
+        bst.insertNode(line);
     }
     fin1.close();
     bstRead = duration_cast<nanoseconds>(high_resolution_clock::now() - start).count();
 
-    ifstream fin2("codes.txt");
-    while (getline(fin2, line)) {
-        l.push_back(line);
-    }
-    fin2.close();
-    lRead = duration_cast<nanoseconds>(high_resolution_clock::now() - start).count();
-
-    // BST read
-    ifstream fin3("codes.txt");
-    start = high_resolution_clock::now();
-    while (getline(fin3, line)) {
+    auto start = high_resolution_clock::now();
+    while (getline(fin, line)) {
         bst.insertNode(line);
     }
-    fin3.close();
+    fin.close();
     bstRead = duration_cast<nanoseconds>(high_resolution_clock::now() - start).count();
-
- 
-
-
-
-    cout << "\n Milestone 3: Displaying BST Contents (In-Order)" << endl;
-    bst.displayInOrder(); 
-    cout << "End of BST Output\n" << endl;
-    
-    // Output results
-    cout << left << setw(15) << "Operation" << setw(15) << "Vector" << setw(15) << "List" << setw(15) << "BST (Custom)"<< endl;
-    cout << setfill('-') << setw(60) << "" << setfill(' ') << endl;
-
-    cout << left << setw(15) << "Read" 
-         << setw(15) << vRead 
-         << setw(15) << lRead bstRead = duration_cast<nanoseconds>(high_resolution_clock::now() - start).count();
-         
-         << setw(15) << bstRead << endl;
-         
-    cout << left << setw(15) << "Sort" 
-         << setw(15) << vSort 
-         << setw(15) << lSort 
-         << setw(15) << bstSort << endl;
 
     return 0;
 }
