be27df63e81169a0774b7e7e1f3f664e78e07375
llt3055 <llt30555@gmail.com>
2026-05-13 19:59:02 -0700

    add sth

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -32,12 +32,23 @@ int main() {
     fin1.close();
     bstRead = duration_cast<nanoseconds>(high_resolution_clock::now() - start).count();
 
-    auto start = high_resolution_clock::now();
-    while (getline(fin, line)) {
-        bst.insertNode(line);
-    }
-    fin.close();
-    bstRead = duration_cast<nanoseconds>(high_resolution_clock::now() - start).count();
+    int choice;
+    string entry;
+
+    do {
+        cout << "\n--- Binary Tree Management Menu ---" << endl;
+        cout << "1. Add a record" << endl;
+        cout << "2. Delete a record" << endl;
+        cout << "3. Search for a record" << endl;
+        cout << "4. Modify a record" << endl;
+        cout << "5. Display all records (In-Order)" << endl;
+        cout << "6. Exit" << endl;
+        cout << "Enter your choice: " << endl;
+        cin >> choice;
+
+
+        
+    } while (choice = 6);
 
     return 0;
 }
